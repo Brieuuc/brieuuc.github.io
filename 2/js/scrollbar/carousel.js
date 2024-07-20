1b1755cc167de167cf9e6ed02f0781ad8e46f33f
Brieuuc <118290382+Brieuuc@users.noreply.github.com>
2024-07-20 15:01:56 +0200

    Add files via upload

---
+++ 2/js/scrollbar/carousel.js
@@ -0,0 +1,10 @@
+import Splide from "@splidejs/splide";
+import '@splidejs/splide/css';
+var splide = new Splide( '.splide', {
+      type     : 'loop',
+      height   : '10rem',
+      focus    : 'center',
+      autoWidth: true,
+    } );
+    
+    splide.mount();
